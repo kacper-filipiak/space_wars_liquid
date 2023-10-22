160df1f4fb1a18600a63e56fcb3d10045c941cb1
Kacper Filipiak <kisielWrosole@gmail.com>
2023-10-22 22:30:01 +0200

    Added user interface

--- kotlin/space_wars/src/main/kotlin/Main.kt
+++ kotlin/space_wars/src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.NumberFormatException
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val calculator = CalculatorImpl();
+    println("Welcome to the Space Wars")
+    while (true) {
+        println("Insert boxes sizes, and press enter to calculate spaceship capacity (egz. 2, 0, 4, 0, 1 ):")
+        try {
+            val input = readln().split(',').map { it.trim().toInt() }
+            val result = calculator.space(input)
+            println("Yours ship capacity is equal $result")
+            println("Do You want to calculate one more time? If yes write \"y\" and pres Enter")
+            if (readln().trim() != "y") {
+                break;
+            }
+        } catch (e: NumberFormatException) {
+           println("Wrong format of input! Try again")
+        }
+    }
 }
